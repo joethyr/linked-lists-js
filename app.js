539af66ac98d804c6a57ecd2d484bb874496350d
joethyr <joethayr@gmail.com>
2023-02-16 14:23:40 -0700

    add remaining functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-
 const linkedLists = (head = null) => {
 
-  const getHead = () => head.value
+  // const getHead = () => head.value
+  const getHead = () => head
 
   // adds a new node containing value to the END of list
   const append = (value) => {
@@ -19,7 +19,7 @@ const linkedLists = (head = null) => {
     }
   }
 
-  // adds a new node containing value to the END of list
+  // adds a new node containing value to the start of list
   const prepend = (value) => {
     const newNode = node(value, head)
     head = newNode
@@ -33,7 +33,6 @@ const linkedLists = (head = null) => {
       count++;
       current = current.nextNode
     }
-    console.log(`size = ${count}`);
     return count;
   }
 
@@ -50,14 +49,61 @@ const linkedLists = (head = null) => {
       return console.log(`There is no node at index ${index}`)
     }
     let node = head
-    if (node.value == null) return
     for (let i = 1; i < index; i++) {
       node = node.nextNode
     }
     return node.value
   }
 
-  return { append, prepend, size, getHead, getTail, atIndex }
+  // removes last node from the list
+  const pop = () => {
+    if (!head) return null
+    let tailNeighbor = head
+    while (tailNeighbor.nextNode.nextNode !== null) {
+      tailNeighbor = tailNeighbor.nextNode
+    }
+    tailNeighbor.nextNode = null
+  }
+
+  const contains = (value) => {
+    if (!head) return null
+    let nodePointer = head
+    while (nodePointer.nextNode !== null) {
+      if (nodePointer.value === value) return true;
+      nodePointer = nodePointer.nextNode
+    }
+    return nodePointer.value === value
+  }
+
+  const find = (value) => {
+    if (!head) return null
+    let index = 1
+    let nodePointer = head
+    while (nodePointer.nextNode !== null) {
+      if (nodePointer.value === value) return index;
+      nodePointer = nodePointer.nextNode
+      index++
+    }
+    return nodePointer.value === value ? index : null
+  }
+
+  // ( value ) -> ( value ) -> ( value ) -> null
+  const toString = () => {
+    let str = ""
+    if (!head) return null
+    let currentNode = head
+    while (currentNode.nextNode !== null) {
+      const newStr = `( ${currentNode.value} ) -> `
+      str += newStr
+      currentNode = currentNode.nextNode
+    }
+    str += (`( ${currentNode.value} ) --> null`);
+    console.log(str);
+    return str;
+  }
+
+
+  return { append, prepend, size, getHead, getTail, atIndex, pop, contains, find, toString }
 }
 
 const node = (value = null, nextNode = null) => ({ value, nextNode })
@@ -67,7 +113,16 @@ test.append(1)
 test.append(4)
 test.append(6)
 test.prepend(7)
-test.size()
+console.log(`The Size is ${test.size()}`)
 // console.log(test.getHead())
 // console.log(test.getTail())
-console.log(test.atIndex(10))
+// console.log(test.atIndex(1))
+// console.log(test.atIndex(2))
+// console.log(test.atIndex(3))
+// console.log(test.atIndex(6))
+// console.log(test.atIndex(4))
+// test.pop()
+// console.log(test.getHead())
+// console.log(test.contains(1))
+// console.log(test.find(4))
+console.log(test.toString())
